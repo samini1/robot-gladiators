ed1015365eddc2ade4f8ee6c752f589d384c4bd8
samini1 <92944248+samini1@users.noreply.github.com>
2021-11-12 16:34:39 -0600

    3.15

---
+++ assets/js/game.js
@@ -0,0 +1,15 @@
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+//fight();
+
+var playerName = window.prompt("What is your robot's name");
+//What is this?
+console.log(playerName);
+console.log(10+10);
+console.log("Our robot's name is " + playerName);
+
+var name = "your name";
+console.log(name);
